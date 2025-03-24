5f7d496234cbe886586b060a8abb17e7911f50fc
changang-4life <163348160+changang-4life@users.noreply.github.com>
2025-03-25 08:43:17 +1300

    Version 1

---
+++ burger_combo_v1.py
@@ -0,0 +1,30 @@
+"""Program which stores a quick menu of the combo meals for a friend's
+takeaway business
+Jade Akinbo
+v1 - main_routine() and Menu Details
+"""
+
+menu = \
+{
+    'Value' : {
+        'Beef Burger' : '$5.69',
+        'Fries' : '$1.00',
+        'Fizzy Drink' : '$1.00'
+    },
+    'Cheezy' : {
+        'Cheeseburger': '$6.69',
+        'Fries' : '$1.00',
+        'Fizzy Drink' : '$1.00'
+    },
+    'Super' : {
+        'Cheeseburger' : '$6.69',
+        'Large Fries' : '$2.00',
+        'Smoothie' : '$2.00'
+    }
+}
+
+def main_routine():
+    """Main Routine"""
+    print("Welcome")
+
+main_routine()
